f4e1b5ac7e0fc8bd3391b686d00bb31f8d2d22bd
Mon Hải <haiphung98st@gmail.com>
2024-07-10 21:49:55 +0700

    change config API

--- NeighborlyAPI/getAdvertisement/__init__.py
+++ NeighborlyAPI/getAdvertisement/__init__.py
@@ -1,6 +1,7 @@
 import azure.functions as func
 import pymongo
 import json
+import os
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 import logging
@@ -14,7 +15,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     
     if id:
         try:
-            url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+            url = os.environ["MyDbConnection"]
             client = pymongo.MongoClient(url)
             database = client['azure']
             collection = database['advertisements']
